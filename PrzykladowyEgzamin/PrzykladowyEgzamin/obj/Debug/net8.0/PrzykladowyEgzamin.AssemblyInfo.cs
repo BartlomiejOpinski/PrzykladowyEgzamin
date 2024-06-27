50081f08bc535664eb9b16fc9a647af30c5b3f33
s18684 <s18684@pjwstk.edu.pl>
2024-06-27 19:50:22 +0200

    notatki i nowa migracja

--- PrzykladowyEgzamin/PrzykladowyEgzamin/obj/Debug/net8.0/PrzykladowyEgzamin.AssemblyInfo.cs
+++ PrzykladowyEgzamin/PrzykladowyEgzamin/obj/Debug/net8.0/PrzykladowyEgzamin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrzykladowyEgzamin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5abcd6c3366cdbca83967437a797e2f7cf58e30c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd0e7af87472fc3aac8a414d1539060a8603369")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrzykladowyEgzamin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrzykladowyEgzamin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
